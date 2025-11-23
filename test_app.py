acc435b5c5dd59a16dbd5ea0a7947b3752947fa1
Faraz <faraz.e.ahangar@gmail.com>
2025-11-23 01:19:28 -0800

    Add files via upload

---
+++ test_app.py
@@ -0,0 +1,71 @@
+#from pycaiso.oasis import Node
+from datetime import datetime
+import pandas as pd
+from typing import List, Dict, TypeVar, Union, Optional, Any
+#from pycaiso.oasis import Atlas, BadDateRangeError, Node, Oasis, SystemDemand, get_lmps
+import time
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+# select pnode
+
+import dash
+from dash import dcc, html, Input, Output
+import pandas as pd
+import plotly.express as px
+
+import streamlit as st
+
+import warnings
+
+# Ignore all warnings
+warnings.filterwarnings("ignore")
+
+combined_df = pd.read_csv('demand_2019_2025.csv')
+
+
+
+
+combined_df['Datetime'] = pd.to_datetime(combined_df['Date'].astype(str) + ' ' + combined_df['Time'], format='%Y-%m-%d %H:%M')
+
+# Extract additional time features
+combined_df['Year'] =combined_df['Datetime'].dt.year
+combined_df['Month'] = combined_df['Datetime'].dt.month
+combined_df['Day'] = combined_df['Datetime'].dt.day
+combined_df['Hour'] = combined_df['Datetime'].dt.hour
+combined_df['Time'] = combined_df['Datetime'].dt.time
+
+
+
+# Group by Hour and compute average demand for each grouping
+monthly_avg = combined_df.groupby(['Month', 'Time'])['Current demand'].mean().reset_index()
+yearly_avg = combined_df.groupby(['Year', 'Time'])['Current demand'].mean().reset_index()
+yearly_month_avg = combined_df.groupby(['Year','Month', 'Time'])['Current demand'].mean().reset_index()
+
+
+# Group by Hour and compute average demand for each grouping
+yearly_monthly_avg = combined_df.groupby(['Year', 'Time'])['Current demand'].mean().reset_index()
+
+
+
+
+#--- Plot 1: Value vs Time for a chosen Month, colored by Year ---
+f1 = yearly_monthly_avg.copy()
+f1 = f1.sort_values(["Time", "Year"])
+
+fig1 = px.line(
+    f1,
+    x="Time",
+    y="Current demand",
+    color="Year",
+    color_discrete_sequence=px.colors.sequential.algae,
+    markers=False
+    )
+
+fig1.update_layout(xaxis_title="Time", yaxis_title="Demand", legend_title="Year")
+fig1.update_xaxes(type="category", tickangle=-45)
+
+
+
+# --- Layout ---
+st.subheader("Historical Changes in the Duck Curve (Demand Curve)")
+st.plotly_chart(fig1, use_container_width=True)
