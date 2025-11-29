9b535ad8d1902a494923df27cf9c6ce5359893b3
Faraz <faraz.e.ahangar@gmail.com>
2025-11-29 04:05:50 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,20 +5,15 @@ from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.express as px
-
 import streamlit as st
-
 import warnings
-# Ignore all warnings
 warnings.filterwarnings("ignore")
 
 
 # # 1- Download Data
 # Downloading Historical Data for Net Demand and Fuel Sources.
 
-# In[2]:
-
-
+#function to download CAISO data
 def download_caiso_data(start_date, end_date, target='netdemand'):
     #target options are: demand, netdemand, fuelsource
     
@@ -43,49 +38,30 @@ def download_caiso_data(start_date, end_date, target='netdemand'):
         print("No data was loaded.")
         return pd.DataFrame()
 
-
-# In[4]:
-
-
-#download net demand data
-start_date = datetime(2019, 1, 1)
-end_date = datetime(2025, 11, 1)
-
-
-# # Download the netdemand data
-# netdemand = download_caiso_data(start_date, end_date, target='netdemand')
-# netdemand[['Time','Current demand','Net demand','Date']].to_csv('netdemand_2019_2025.csv', index = False)
-# 
-
-# # Download fuelsource data
-# fuelsource = download_caiso_data(start_date, end_date, target='fuelsource')
-# 
-# # Combining Large Hydro and Natural Gas columns into one
-# fuelsource.loc[fuelsource['Natural gas'].isna(),
-#                'Natural gas']  = fuelsource.loc[fuelsource['Natural gas'].isna(),'Natural Gas']
-# 
-# 
-# fuelsource.loc[fuelsource['Large hydro'].isna(),
-#                'Large hydro']  = fuelsource.loc[fuelsource['Large hydro'].isna(),'Large Hydro']
-# 
-# fuelsource.drop(columns= ['Natural Gas', 'Large Hydro'], inplace= True)
-# 
-# fuelsource.to_csv('fuelsource_2019_2025.csv', index = False)
-# 
-
-# In[35]:
+#-----Download the netdemand and fuelsource data:start- comment out if already downloaded
+start_date = datetime(2019, 1, 1) #start date for download
+end_date = datetime(2025, 11, 1)  #end date for download
+#netdemand = download_caiso_data(start_date, end_date, target='netdemand') #download netdemand
+#netdemand[['Time','Current demand','Net demand','Date']].to_csv('netdemand_2019_2025.csv', index = False) #save as csv
+#fuelsource = download_caiso_data(start_date, end_date, target='fuelsource') #download fuelsource
+# Combining Large Hydro and Natural Gas columns into one
+#fuelsource.loc[fuelsource['Natural gas'].isna(),
+ #              'Natural gas']  = fuelsource.loc[fuelsource['Natural gas'].isna(),'Natural Gas']
+#fuelsource.loc[fuelsource['Large hydro'].isna(),
+ #              'Large hydro']  = fuelsource.loc[fuelsource['Large hydro'].isna(),'Large Hydro']
+#fuelsource.drop(columns= ['Natural Gas', 'Large Hydro'], inplace= True)
+#fuelsource.to_csv('fuelsource_2019_2025.csv', index = False) #save as csv
+#-----Download the netdemand and fuelsource data: end
 
 
+#Read downloaded files if available
 fuelsource = pd.read_csv('fuelsource_2019_2025.csv')
 netdemand = pd.read_csv('netdemand_2019_2025.csv')
 
 
 # # 2- Monthly and Daily Averages
 
-# In[36]:
-
-
-# Function to add ordinal suffix
+# Function to add ordinal date
 def add_ordinal(n):
     if 11 <= n % 100 <= 13:
         suffix = 'th'
@@ -104,37 +80,23 @@ def extract_time_features(df):
     # Create new column with formatted date
     df['Day'] = df['Datetime'].apply(lambda x: f"{x.strftime('%B')} {add_ordinal(x.day)}")
 
-
-# In[37]:
-
-
 extract_time_features(fuelsource)
-
-
-# In[38]:
-
-
 extract_time_features(netdemand)
-
-
-# In[39]:
-
 
 #Monthly Averages
 netdemand_yearly_monthly = netdemand.groupby(['Year','Month', 'Time'])['Net demand'].mean().reset_index()
 fuelsource_yearly_monthly = fuelsource.groupby(['Year','Month', 'Time'])[['Solar','Wind','Natural gas']].mean().reset_index()
-fuelsource_yearly_monthly['Solar_Wind'] = fuelsource_yearly_monthly['Solar'] + fuelsource_yearly_monthly['Wind']
+fuelsource['Solar_Wind'] = fuelsource['Solar'] + fuelsource['Wind']
 
 
-# # 3- Plots
-
-# In[40]:
-
+# # 3- Dashboard and Plots
 
 st.markdown("""
 # What's with the&nbsp;Duck?!
 ## Duck Curve and Its&nbsp;Changes
+""", unsafe_allow_html=True)
 
+st.markdown("""
 ### Why the Duck Curve?
 As renewable energy sources, especially solar power, grows in many utility grid systems, operating conditions increasingly depend on controllable generation rather than raw demand. Traditional power plants - such as gas and coal - can adjust their output relatively quickly, but solar and wind resources cannot be turned on or off at will. This introduces new challenges for grid operators.
 
@@ -143,39 +105,56 @@ Electricity demand typically ramps up in the morning as people go to work and bu
 At night, while the demand and solar generation are both low, net load remains relatively stable. Starting the day, the demand increases and the Net Load increases too. In regions with high solar penetration - such as California - the net load dips sharply in the middle of the day due to abundant solar production. California Independent System Operator (CAISO) popularized the term "Duck Curve" because the midday dip makes the graph look like a duck [1].
 """, unsafe_allow_html=True)
 
-
-netdemand.tail(1)
-
-
-# In[64]:
-
-
-# Plot the April Changes
-#--- Plot 1: Value vs Time for a chosen Month, colored by Year ---
-# --- Controls ---
+#--- Figure 1: Net Demand vs Time for a chosen day as well as solar+wind generation
 date_choice = st.date_input("Select a date",
                     min_value=start_date,
                     max_value= end_date)
+
+date_choice = datetime(2025,6,2)
+netdemand_date = netdemand[netdemand["Date"] == pd.to_datetime(date_choice)].copy()
 fig1 = px.line(
-    netdemand[netdemand["Date"] == pd.to_datetime(date_choice)].copy(),
+    netdemand_date,
     x="Time",
     y="Net demand",
-    color="Date",
+    color=px.Constant("Net Demand"),
     markers=False,
-    color_discrete_sequence=px.colors.sequential.algae,
-    title=f"Net Demand vs Time — April 4th 2025",
+    #color_discrete_sequence=px.colors.sequential.algae,
+    #title=f"Net Demand vs Time for Date: {date_choice}",
 )
-fig1.update_layout(xaxis_title="Time of Day", yaxis_title="MW", legend_title="Year")
+
+fig1.update_traces(line=dict(color="green"))
+fig1.update_layout(xaxis_title="Time of Day", yaxis_title="MW", legend_title="Legend")
 fig1.update_xaxes(type="category", tickangle=-70)
+
+
+# Overlay from fuelsource (Solar_Wind vs Time)
+fuelsource_date = fuelsource[fuelsource["Date"] == pd.to_datetime(date_choice)].copy()
+
+fig1.add_scatter(
+    x=fuelsource_date["Time"],
+    y=fuelsource_date["Solar_Wind"],
+    mode="lines",
+    name="Solar + Wind",
+    line=dict(color="red", width=2, dash="dot")  # Customize style
+)
 
 st.plotly_chart(fig1)
 #fig1.show()
 
+st.markdown("""
+### Challenges
+High solar adoption - and the resulting larger belly in the net load curve - has introduced significant challenges for system operators. When the sun sets, solar generation rapidly declines, causing the net load to rise sharply. This creates a need for additional flexibility and fast ramp-up of conventional generation in the evening. Such steep ramps can lead to increased wear on generation units and reduced efficiency, particularly for coal-fired thermal plants [2].
 
-# In[63]:
+Another challenge occurs during extreme duck curve conditions when renewable production exceeds demand in the afternoon. This surplus can lead to negative net load and sometimes even negative electricity prices, meaning California sometimes pays other states to absorb excess power [3].
 
+These challenges can be mitigated through by accurate solar generation forecasting, increased energy storage capacity, or enhanced system flexibility and demand response programs [4]
 
-# Plot Different sources compared to each other
+---
+
+### Changes of the Duck Curve
+In 2024, solar energy (utility and smaller scale) supplied about 32% of California's total electricity net generation [5]. The plot below illustrates how electricity generation has evolved in California between 2019 and 2025. As the solar generation has been increasing, the natural gas and import portion of electricity sources have been decreases.
+""", unsafe_allow_html=True)
+
 
 # Plot Different sources compared to each other
 
@@ -203,10 +182,9 @@ fig2 = px.area(long_df, x='Date', y='Generation', color='Source',
 st.plotly_chart(fig2)
 #fig2.show()
 
-#fig.show()
-
-
-# In[45]:
+st.markdown("""
+With rising solar and wind generation, the duck curve has become increasingly pronounced. Here, I visualize these changes in California using data from CAISO. Notice how the net-load curve drops lower during midday each month as more solar capacity is added to the grid. During the spring months, the average net demand curve is negative during the 2025.
+""", unsafe_allow_html=True)
 
 
 # --- Controls ---
@@ -242,9 +220,17 @@ st.plotly_chart(fig3)
 
 #fig3.show()
 
+st.markdown("""
+### Future of the Duck
+In many places like California, solar and wind are the cheapest of power sources [6]. The growing solar penetration reshapes the grid and the net-load curve, with its impact increasing over time. The Duck Curve will keep deepening, with negative net loads becoming common. Ultimately, the deepening Duck Curve is a clear indicator of the energy transition ahead, where adaptability and innovation will define the success of a renewable-powered grid.
 
+---
 
-
-
-
-
+### Reference
+[1] California Independent System Operator. (2016). Flexible resources help renewables: Fast facts. Retrieved from https://www.caiso.com/Documents/FlexibleResourcesHelpRenewables_FastFacts.pdf  
+[2] Bird, L., Milligan, M., &amp; Lew, D. (2013). Integrating variable renewable energy: Challenges and solutions (Technical Report NREL/TP-6A20–60451). National Renewable Energy Laboratory. Retrieved from https://docs.nrel.gov/docs/fy13osti/60451.pdf  
+[3] Bhandari, D. (2025, April 15). Negative prices in CAISO: What PPA buyers and renewable developers need to know. Renewable Energy World. Retrieved from https://www.renewableenergyworld.com/solar/negative-prices-in-caiso-what-ppa-buyers-and-renewable-developers-need-to-know/  
+[4] U.S. Energy Information Administration. (2023, June 21). As solar capacity grows, duck curves are getting deeper in California. Today in Energy. Retrieved from https://www.eia.gov/todayinenergy/detail.php?id=56880  
+[5] U.S. Energy Information Administration. (2025, June 20). California: State energy profile analysis. Retrieved from https://www.eia.gov/state/analysis.php?sid=CA  
+[6] Plumer, B. (2025, March 17). A Trump overhaul of the Energy Dept. breaks up clean energy offices. The New York Times. Retrieved from https://www.nytimes.com/2025/03/17/climate/renewable-energy-trump-electricity.html  
+""", unsafe_allow_html=True)
