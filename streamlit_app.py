70f5fc8d3b2db0f8881a7e1c551f22a5c1a004b4
Faraz <faraz.e.ahangar@gmail.com>
2025-11-30 01:17:16 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import streamlit as st
 import warnings
 warnings.filterwarnings("ignore")
 
-# # 1- Download Data----------------------------------
+# # 1- Download Data from CAISO ----------------------------------
 
 # Downloading Historical Data for Net Demand and Fuel Sources
 #function to download CAISO data
@@ -39,7 +39,7 @@ def download_caiso_data(start_date, end_date, target='netdemand'):
 start_date = datetime(2019, 1, 1) #start date for download
 end_date = datetime(2025, 10, 31)  #end date for download
 
-#-----Download the netdemand and fuelsource data:start- comment out if already downloaded
+#-----Download the netdemand and fuelsource data for California from CAISO:start- comment out if already downloaded
 #netdemand = download_caiso_data(start_date, end_date, target='netdemand') #download netdemand
 #netdemand[['Time','Current demand','Net demand','Date']].to_csv('netdemand_2019_2025.csv', index = False) #save as csv
 #fuelsource = download_caiso_data(start_date, end_date, target='fuelsource') #download fuelsource
@@ -83,13 +83,19 @@ st.markdown("""
 ## How Solar Generation Reshaped the Net Load&nbsp;Curve
 """, unsafe_allow_html=True)
 
+st.image('image_intro.png')
+
 st.markdown("""
-### Why the Duck Curve?
-As renewable energy sources, especially solar power, grows in many utility grid systems, operating conditions increasingly depend on controllable generation rather than raw demand. Traditional power plants - such as gas and coal - can adjust their output relatively quickly, but solar and wind resources cannot be turned on or off at will. This introduces new challenges for grid operators.
+
+## Why the Duck Curve?
+
+The "Duck Curve" is a graph that shows how solar power changes electricity demand throughout the day, creating a shape that looks like a duck.
+
+As renewable energy sources, especially solar power, grow in many utility grid systems, operating conditions increasingly depend on controllable generation rather than raw demand. Traditional power plants - such as gas and coal - can adjust their output relatively quickly, but solar and wind resources cannot be turned on or off at will. This introduces new challenges for grid operators.
 
 Electricity demand typically ramps up in the morning as people go to work and businesses open. During midday, when solar generation is high, the system requires significantly less power from conventional sources. What matters most is the total demand minus the output from variable renewable resources (solar and wind), which is called the "Net Load". In other words, net load equals the forecasted demand minus expected generation from solar and wind.
 
-At night, while the demand and solar generation are both low, net load remains relatively stable. Starting the day, the demand increases and the Net Load increases too. In regions with high solar penetration - such as California - the net load dips sharply in the middle of the day due to abundant solar production. California Independent System Operator (CAISO) popularized the term "Duck Curve" because the midday dip makes the graph look like a duck [1].
+At night, while the demand and solar generation are both low, net load remains relatively stable. Starting the day, the demand increases and the net load increases too. In regions with high solar penetration - such as California - the net load dips sharply in the middle of the day due to abundant solar production. California Independent System Operator (CAISO) popularized the term "Duck Curve" because the midday dip makes the graph look like a duck [1].
 """, unsafe_allow_html=True)
 
 #--- Figure 1: Net Demand vs Time for a chosen day as well as solar+wind generation
@@ -97,8 +103,8 @@ date_choice = st.date_input("Select a date",
                     min_value=start_date,
                     max_value= end_date)
 
-date_choice = datetime(2025,6,2)
 netdemand_date = netdemand[netdemand["Date"] == pd.to_datetime(date_choice)].copy()
+#plot net demand (duck curve)
 fig1 = px.line(
     netdemand_date,
     x="Time",
@@ -112,7 +118,6 @@ fig1 = px.line(
 fig1.update_traces(line=dict(color="green"))
 fig1.update_layout(xaxis_title="Time of Day", yaxis_title="MW", legend_title="Legend")
 fig1.update_xaxes(type="category", tickangle=-70)
-
 
 # Overlay from fuelsource (Solar_Wind vs Time)
 fuelsource_date = fuelsource[fuelsource["Date"] == pd.to_datetime(date_choice)].copy()
@@ -129,29 +134,33 @@ st.plotly_chart(fig1)
 #fig1.show()
 
 st.markdown("""
-### Challenges
+
+The effects of the duck curve are most pronounced during springtime in California, when sunny conditions coincide with mild temperatures. During these periods, electricity demand is low because heating and cooling systems are rarely used, yet solar generation remains high - creating steep ramps in net load later in the day.
+
+---
+
+## Challenges
+
 High solar adoption - and the resulting larger belly in the net load curve - has introduced significant challenges for system operators. When the sun sets, solar generation rapidly declines, causing the net load to rise sharply. This creates a need for additional flexibility and fast ramp-up of conventional generation in the evening. Such steep ramps can lead to increased wear on generation units and reduced efficiency, particularly for coal-fired thermal plants [2].
 
 Another challenge occurs during extreme duck curve conditions when renewable production exceeds demand in the afternoon. This surplus can lead to negative net load and sometimes even negative electricity prices, meaning California sometimes pays other states to absorb excess power [3].
 
-These challenges can be mitigated through by accurate solar generation forecasting, increased energy storage capacity, or enhanced system flexibility and demand response programs [4]
+These challenges can be mitigated through accurate solar generation forecasting, increased energy storage capacity, or enhanced system flexibility and demand response programs [4]
 
 ---
 
-### Changes of the Duck Curve
+## Changes of the Duck Curve
+
 In 2024, solar energy (utility and smaller scale) supplied about 32% of California's total electricity net generation [5]. The plot below illustrates how electricity generation has evolved in California between 2019 and 2025. As the solar generation has been increasing, the natural gas and import portion of electricity sources have been decreases.
 """, unsafe_allow_html=True)
 
-
 # Plot Different sources compared to each other
-
 cols = ['Solar', 'Wind','Batteries', 'Small hydro', 'Large hydro','Geothermal', 'Biomass', 'Biogas',
         'Coal', 'Nuclear', 'Natural gas' ,  'Imports', 'Other']
 
 # Aggregate by Year
 fuelsource_monthly = fuelsource[fuelsource.Date<datetime(2025, 11, 1)].groupby(['Year', 
                                         'Month'])[cols].sum().reset_index().sort_values(['Year', 'Month'])
-
 
 # Build a proper Date column for the x-axis (first day of each month)
 fuelsource_monthly['Date'] = pd.to_datetime(dict(year=fuelsource_monthly['Year'], month=fuelsource_monthly['Month'], day=1))
@@ -170,17 +179,16 @@ st.plotly_chart(fig2)
 #fig2.show()
 
 st.markdown("""
-With rising solar and wind generation, the duck curve has become increasingly pronounced. Here, I visualize these changes in California using data from CAISO. Notice how the net-load curve drops lower during midday each month as more solar capacity is added to the grid. During the spring months, the average net demand curve is negative during the 2025.
+With rising solar and wind generation, the duck curve has become increasingly pronounced. Here, I visualize these changes in California using data from CAISO. Notice how the net-load curve drops lower during midday each month as more solar capacity is added to the grid. During the spring months of 2025, the average net demand curve becomes negative in the afternoon.
 """, unsafe_allow_html=True)
 
-
+# plot historical monthly duck curve for different months 
 # --- Controls ---
 available_months = [m for m in netdemand_yearly_monthly["Month"].dropna().unique()]
 available_months = sorted([int(m) for m in available_months])
 month_names = {i: ["January","February","March","April",
                    "May","June","July","August","September",
                    "October","November","December"][i-1] for i in range(1,13)}
-
 
 month_choice_num = st.selectbox(
     "Select Month",
@@ -189,8 +197,7 @@ month_choice_num = st.selectbox(
 )
 
 
-#--- Plot 2: Net-demand vs Time for a chosen Month, colored by Year ---
-
+# Net-demand vs Time for a chosen Month, colored by Year ---
 fig3 = px.line(
     netdemand_yearly_monthly[netdemand_yearly_monthly["Month"] == month_choice_num].copy(),
     x="Time",
@@ -221,4 +228,5 @@ In many places like California, solar and wind are the cheapest of power sources
 [5] U.S. Energy Information Administration. (2025, June 20). California: State energy profile analysis. Retrieved from https://www.eia.gov/state/analysis.php?sid=CA  
 [6] Plumer, B. (2025, March 17). A Trump overhaul of the Energy Dept. breaks up clean energy offices. The New York Times. Retrieved from https://www.nytimes.com/2025/03/17/climate/renewable-energy-trump-electricity.html  
 """, unsafe_allow_html=True)
+
 
